148a847a7ac979f80a6802dc7c7bd9134305a7b3
JaeEon Hwang <hju753@naver.com>
2024-04-22 23:00:28 +0900

    백준 2457 완료

--- Baekjoon/BJ2457.py
+++ Baekjoon/BJ2457.py
@@ -1,32 +1,28 @@
 import sys
 sys.stdin = open('input.txt', 'r')
 
+days = {1: 31, 2: 28, 3: 31, 4: 40, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
 N = int(input())
 dates = []
-flowers = []
 for _ in range(N):
-    date = list(map(int, input().split()))
-    if date[0] < 3 or date[0] == 3 and date[1] == 1:
-        flowers.append([date])
+    date = tuple(map(int, input().split()))
     dates.append(date)
 dates.sort(key=lambda x: (x[2], x[3]), reverse=True)
+start = (3, 1)
+ans = 0
 print(dates)
-print(flowers)
-if not flowers:
-    print(0)
-else:
-    while True:
-        for flower in flowers:
-            idx = 0
-            try:
-                while flower[-1][2] < dates[idx][0] or (flower[-1][2] == dates[idx][0] and flower[-1][3] + 1 < dates[idx][1]):
-                    idx += 1
-                    flower.append(dates[idx])
-            except:
-                pass
-            if dates[idx][2] == 12 or (dates[idx][2] == 11 and dates[idx][3] == 30):
-                print(len(flower))
-                break
-        else:
-            print()
-
+while True:
+    try:
+        idx = 0
+        while (dates[idx][0] == start[0] and dates[idx][1] > start[1]) or dates[idx][0] > start[0]:
+            idx += 1
+        start = (dates[idx][2], dates[idx][3])
+        ans += 1
+        print(start)
+        dates.remove(dates[idx])
+        if start[0] == 12:
+            print(ans)
+            break
+    except:
+        print(0)
+        break
